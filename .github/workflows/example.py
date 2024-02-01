b723fab0b43db580649c008c8b8842144cdfe627
hcr5 <157815647+hcr5@users.noreply.github.com>
2024-02-01 07:10:20 +0100

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,33 +1,5 @@
 def main():
-  import os
-  
-  os.system("pip install -U scratchattach")
-  
-  import scratchattach as scratch3
-  from scratchattach import Encoding
-  import winsound
-  
-  events = scratch3.CloudEvents("669020072")
-  
-  @events.event
-  def on_set(event): #Called when a cloud var is set
-      print(f"{event.user}: {Encoding.decode(event.value)}")
-      winsound.Beep(1000, 200)
-  
-  @events.event
-  def on_del(event):
-      print(f"{event.user} deleted variable {event.var}")
-  
-  @events.event
-  def on_create(event):
-      print(f"{event.user} created variable {event.var}")
-  
-  @events.event #Called when the event listener is ready
-  def on_ready():
-     print("Event listener ready!")
-  
-  events.start()
-
+ print("hello world")
 
 if __name__ == '__main__':
   main()
